b70d2eea5e72d7fb6c8620b64d4a8e8a5529a88d
Denis <denis3901442@gmail.com>
2019-05-19 13:14:05 +0300

    Add default mysql-container with default Database.

--- app/public/index.php
+++ app/public/index.php
@@ -4,8 +4,6 @@ require_once '../vendor/autoload.php';
 
 ini_set('display_errors', 1);
 
-//$test = new App\controllers\TmdbMovies();
+$test = new App\controllers\TmdbMovies();
 
-echo '<pre>';
-var_dump($_ENV);
-var_dump($_SERVER);
+var_dump($test->getMovies());
--- app/src/controllers/TmdbMovies.php
+++ app/src/controllers/TmdbMovies.php
@@ -12,8 +12,7 @@ class TmdbMovies
     public function getMovies()
     {
         $client = new Client();
-        //$result = $client->get('https://api.themoviedb.org/3/movie/551?api_key=e3c790bdb811cade513e875f4806841d');
-        $result = $client->get('http://test.digipluggen.nl/denis_response_1.json');
+        $result = $client->get('https://api.themoviedb.org/3/movie/551?api_key=' . getenv("TMDB_API_KEY"));
         return json_decode($result->getBody(), true);
     }
 }
