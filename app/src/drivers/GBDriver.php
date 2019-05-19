a6ee4494cdd4133fa7e33aed69220af75ba9f6c3
Denis <denis3901442@gmail.com>
2019-05-19 15:51:42 +0300

    Add Faker server to simulate 3rd party calls

--- app/src/drivers/GBDriver.php
+++ app/src/drivers/GBDriver.php
@@ -1,15 +1,14 @@
 <?php
 
-
 namespace App\drivers;
 
 use App\interfaces\PhoneValidatorDriver;
 
-class MelisaDriver implements PhoneValidatorDriver
+class GBDriver implements PhoneValidatorDriver
 {
     public function validate($phone)
     {
-        var_dump('from melisa ' . $phone);
+        var_dump('hello from GB ' . $phone);
     }
 
     public function prepareResult()
